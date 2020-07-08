70287932c4c90213436fb42c46da47f55f34061e
Don Mallory <dmm333@gmail.com>
2020-07-07 21:55:33 -0500

    https

--- src/pages/wines-panel/WinesPanel.js
+++ src/pages/wines-panel/WinesPanel.js
@@ -3,7 +3,8 @@ import NewWineForm from '../../components/new-wine-form/NewWineForm'
 import Wine from '../../components/wine/Wine'
 import { DirectUpload } from '@rails/activestorage'
 import api from '../../services/api'
-const API_ROOT = 'http://ancient-thicket-66765.herokuapp.com'
+const API_ROOT = 'https://ancient-thicket-66765.herokuapp.com'
+// const API_ROOT = 'http://localhost:3000'
 
 export class WinesPanel extends PureComponent {
   state = {
@@ -18,10 +19,12 @@ export class WinesPanel extends PureComponent {
   }
   
   directUploadComplete = (error, response) =>{
-    this.setState({
-      hasImageAttached: (!error && !!response.key),
-      image: response.key || null
-    })
+    if (!error) { //only set state if returned properly
+      this.setState({
+        hasImageAttached: true,
+        image: response.key
+      })
+    }
   }
 
 
