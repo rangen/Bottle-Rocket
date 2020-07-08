86f83d118861a340030b42251bf9d5ed29a95c5b
Don Mallory <dmm333@gmail.com>
2020-07-08 15:08:38 -0500

    header

--- src/pages/wines-panel/WinesPanel.js
+++ src/pages/wines-panel/WinesPanel.js
@@ -5,8 +5,8 @@ import { Button } from '@material-ui/core'
 import './wines-panel.styles.scss'
 import { DirectUpload } from '@rails/activestorage'
 import api from '../../services/api'
-const API_ROOT = 'https://ancient-thicket-66765.herokuapp.com'
-// const API_ROOT = 'http://localhost:3000'
+// const API_ROOT = 'https://ancient-thicket-66765.herokuapp.com'
+const API_ROOT = 'http://localhost:3000'
 
 export class WinesPanel extends PureComponent {
   state = {
